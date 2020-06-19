e7e690a744e452918cd48272c6537c9315c1dfdc
Marlo van Gulik <mjvangulik208@gmail.com>
2020-06-19 15:20:35 +0200

    Created file uploading system

--- view/games/suggest.php
+++ view/games/suggest.php
@@ -1,9 +1,10 @@
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
     <input type="text" name="gamename" placeholder="Game name">
     <textarea name="description" placeholder="Description"></textarea>
     <input type="text" name="developer" placeholder="Developer">
     <input type="date" name="released" placeholder="Date">
     <input type="text" name="platform" placeholder="Platform">
     <input type="number" name="PEGI" placeholder="PEGI code">
+    <input type="file" name="imgFile">
     <input type="submit" value="Submit suggestion">
 </form>
