42300b806440ddcb8446ce211245426ce00ae13d
99009744 <55435447+99009744@users.noreply.github.com>
2020-06-24 11:29:21 +0200

    outline details pagina
    details pagina basis gemaakt

--- view/games/details.php
+++ view/games/details.php
@@ -1,16 +1,19 @@
 <div class="container col-12">
-	<div class="col-12 row">
-        <img src="<?= URL ?>public/images/games/<?= $game[0]['id'] ?>" class="col-4">
-        <div class="col-3 bg-secondary text-light">
-            <p>Genre: <?= $game[0]['tag'] ?></p>
-            <p>Release date: <?= $game[0]['released'] ?> </p>
-            <p>Developer: <?= $game[0]['developer'] ?> </p>
-            <p><?= $game[0]['platform'] ?> </p>
+<a href="">Back to genres</a>
+	<div class="row">
+        <img src="<?= URL ?>public/images/games/<?= $game[0]['id'] ?>" class="col-3">
+        <div class="col-8">
+            <div class="col-12 bg-dark text-light p-2">
+                <h1 class="text-center"><?= $game[0]['gamename']  ?> </h1>
+                <p><?= $game[0]['description'] ?> </p>
+            </div>
+            <div class="col-12 bg-secondary text-light p-2">
+                <p>Genre: <?= $game[0]['tag'] ?></p>
+                <p>Release date: <?= $game[0]['released'] ?> </p>
+                <p>Developer: <?= $game[0]['developer'] ?> </p>
+                <p>Platform: <?= $game[0]['platform'] ?> </p>
 
-        </div>
-        <div class="col-4 bg-dark text-light">
-            <h1><?= $game[0]['gamename']  ?> </h1>
-            <p> Info: <?= $game[0]['description'] ?> </p>
+            </div>
         </div>
 
     </div>
